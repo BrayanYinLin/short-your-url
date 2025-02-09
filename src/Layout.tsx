e73503ec45e5c07686ed197b06e659ae55a0ee22
Brayan Yin Lin <brayanyl250206@gmail.com>
2025-02-08 23:51:20 -0500

    fix: routes hierarchy

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,19 +6,21 @@ import SignIn from './modules/auth/pages/SignIn'
 import CallbackGithubPage from './modules/auth/pages/CallbackGithub'
 import './index.css'
 import { Dashboard } from './modules/dashboard/pages/Dashboard'
-import { Links } from 'modules/dashboard/pages/Links'
 import CallbackGoogle from 'modules/auth/pages/CallbackGoogle'
+import { Redirect } from 'modules/link/pages/Redirect'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<SignIn />} />
+      <Route>
+        <Route index element={<Home />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path=":short" element={<Redirect />} />
+      </Route>
+
       <Route path="/auth/github/callback" element={<CallbackGithubPage />} />
       <Route path="/auth/google/callback" element={<CallbackGoogle />} />
-      <Route path="/dashboard" element={<Dashboard />}>
-        <Route index element={<Links />} />
-      </Route>
     </Routes>
   </BrowserRouter>
 )
--- src/modules/dashboard/pages/Links.tsx
+++
@@ -1,5 +0,0 @@
-import { ListLinks } from '../components/ListLinks'
-
-export function Links() {
-  return <ListLinks />
-}
