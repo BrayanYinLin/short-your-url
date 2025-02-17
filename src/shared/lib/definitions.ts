9e596fa7ec4d4143b41916ee771ef5ca7e7e1c91
Brayan Yin Lin <brayanyl250206@gmail.com>
2025-02-17 01:40:39 -0500

    fix: redirecctions

--- src/shared/lib/definitions.ts
+++ src/shared/lib/definitions.ts
@@ -1,8 +1,11 @@
 export const ENDPOINTS = {
-  AUTH: 'http://localhost:5373/api/auth/',
-  LINKS: 'http://localhost:5373/api/link/'
+  // AUTH: 'http://localhost:5373/api/auth/',
+  // LINKS: 'http://localhost:5373/api/link/'
+  AUTH: 'https://cool-shortener-production.up.railway.app/api/auth/',
+  LINK: 'https://cool-shortener-production.up.railway.app/api/link/'
 }
 
 export const ORIGINS = {
-  LOCAL: 'http://localhost:5173/'
+  LOCAL: 'https://best-shorter.vercel.app/',
+  DEPLOY: 'http://localhost:5173/'
 }
